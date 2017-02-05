3218ef305f077b5a9b5c77d33facca53b4feb57d
Meghan Horton <meghan.leigh.horton@gmail.com>
2017-02-05 13:37:20 -0500

    Debugging - why can't this work?!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// // set variables for environment
+// set variables for environment
 var express = require('express'),
 	exphbs  = require('express-handlebars'),
 	path = require('path'),
@@ -12,21 +12,19 @@ var express = require('express'),
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// Sets up Handlebars engine with Express
-// Searches for HTML files
+// // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.engine('.html', hbs.engine);
 app.set('view engine', '.html');
-app.enable('view cache');
 
-
-// Render
-app.get('/', function (req, res, next) {
-    res.render('index', {
-    	title: 'Hey'
-    });
+app.get('/', function(request, response) {
+  response.render('index',{
+  	title: 'My first app'
+  });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+console.log(__dirname);
